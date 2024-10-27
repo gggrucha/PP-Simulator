03a4c7ace5614b14daefa3ce1a2ceceb9070d4ad
gggrucha <999bartek999@gmail.com>
2024-10-27 18:07:57 +0100

    Stwory

---
+++ Creature.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// powyższe do wywalenia
+namespace Simulator;
+internal class Creature
+{
+    //public string name;
+    //public int level;
+
+    public string Name { get; set; } //automatic property
+    public int Level { get; set; } //automatic property
+
+    public Creature(string name, int level=1) { //constructor
+        Name = name;
+        Level = level;
+    }
+
+    public Creature() //empty constructor
+    { 
+        
+    }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+    }
+
+    public string Info
+    {
+        get { return $"{Name} [{Level}]"; }
+    }
+
+
+}
