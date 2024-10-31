1a6ec503cf2ee273828016927e250d05eb5b4f77
gggrucha <999bartek999@gmail.com>
2024-10-31 22:12:08 +0100

    Settery

--- Animals.cs
+++ Animals.cs
@@ -7,7 +7,29 @@ using System.Threading.Tasks;
 namespace Simulator;
 public class Animals
 {
-    public required string Description { get; init; }
+    private string description = "";
+    public required string Description
+    {
+        get { return description; }
+        init
+        {
+            description = value.Trim();
+            if (description.Length < 3)
+            {
+                description = description.PadRight(3, '#');
+            }
+            if (description.Length > 15)
+            {
+                description = description.Substring(0, 15);
+                description = description.Trim();
+                if (description.Length < 3)
+                {
+                    description = description.PadRight(3, '#');
+                }
+            }
+            description = char.ToUpper(description[0]) + description.Substring(1);
+        }
+    } 
     public uint Size { get; set; } = 3;
 
     public string Info
--- Creature.cs
+++ Creature.cs
@@ -1,18 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-// powyższe do wywalenia
-namespace Simulator;
+﻿namespace Simulator;
 internal class Creature
 {
-    //public string name;
-    //public int level;
+    private string name = "Unknown";
+    private int level = 1;
 
-    public string Name { get; set; } //automatic property
-    public int Level { get; set; } //automatic property
+    public string Name 
+    { 
+        get { return name; } 
+        init 
+        { 
+            name = value.Trim();
+            
+            if (name.Length < 3)
+            {
+                string hash = new string('#', 3-name.Length);
+                name = name + hash;
+            }
 
+            char c = name[0];
+            if (char.IsLower(c)==true)
+            {
+                char[] znaki = name.ToCharArray();
+                znaki[0] = char.ToUpper(znaki[0]); //changing 0-th element to UpperCase
+                name = new string(znaki);
+            }
+        }
+    }  //automatic property
+    public int Level 
+    { 
+        get { return level; } 
+        init 
+        { 
+            if (value > 10)
+            {
+                level = 10;
+            }
+            else if(value < 1)
+            {
+                level = 1;
+            }
+            else
+            {
+                level = value;
+            }
+        } 
+    } //automatic property
+    
     public Creature(string name, int level=1) { //constructor
         Name = name;
         Level = level;
@@ -27,7 +60,14 @@ internal class Creature
     {
         Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
     }
-
+    public int Upgrade()
+    {
+        if (level < 10)
+        {
+            level++;
+        }
+        return level;
+    }
     public string Info
     {
         get { return $"{Name} [{Level}]"; }
